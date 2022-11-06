377e9a431b35138918cad73d035d74527eb0733d
Daniil <daniil.s20035@mail.ru>
2022-11-06 13:38:21 +0300

    changes

---
+++ lab06/lab06/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace lab06
+{
+    public class Program
+    {
+        public static async Task Main()
+        {
+            SortedSet<string> countries = new SortedSet<string>();
+            List<Weather> weathers = new List<Weather>();
+            /*for (int i = 0; i < 10; ++i)
+            {
+                weathers.Add(new Weather());
+            }
+            foreach (var weather in weathers)
+            {
+                await weather.genResponse();
+                weather.printInfo();
+            }*/
+            for (int i = 0; i < 20; ++i)
+            {
+                weathers.Add(new Weather());
+            }
+            double average_temp = 0;
+            foreach (var weather in weathers)
+            {
+                await weather.genResponse();
+                average_temp += Convert.ToDouble(weather.Temp);
+                countries.Add(weather.Country);
+            }
+            Console.WriteLine($"Average temperature: {average_temp}");
+            
+            var select_min_max = from weath in weathers
+                                 orderby weath.Temp
+                                 select weath;
+            Console.WriteLine($"Max: {select_min_max.Last()}, Min: {select_min_max.First()}");
+
+            Console.WriteLine($"Countries: {countries.Count}");
+
+            var clear_sky_country = from weath in weathers
+                                    where (weath.Description == "clear sky")
+                                    select weath;
+            Console.WriteLine($"First country with clear sky: {clear_sky_country.First().Country}");
+
+            var rainy_country = from weath in weathers
+                                where (weath.Description == "rain")
+                                select weath;
+            Console.WriteLine($"First country with rain: {rainy_country.First().Description}");
+
+            var few_clouds_country = from weath in weathers
+                                     where (weath.Description == "few clouds")
+                                     select weath;
+            Console.WriteLine($"First country with few clouds: {few_clouds_country.First().Description}");
+        }
+    } 
+}
+
---
+++ lab06/lab06/Weather.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Dynamic;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace lab06
+{
+    public class Weather: IComparable<Weather>
+    {
+        private static string API_key = "b0196f08cb67b9600ecbb6f542b69c81";
+        private string lat_;
+        private string lon_;
+        private IComparable<Weather> comparable;
+
+        public string? Country;
+        public string? Name;
+        public string? Temp;
+        public string? Description;
+
+        public Weather()
+        {
+            lat_ = genLat();
+            lon_ = genLon();
+        }
+
+
+
+        public string genLat()
+        {
+            string lat = "";
+            Random random = new Random();
+            int first = -90;
+            int last = 90;
+            lat = Convert.ToString(random.Next(first, last));
+            return lat;
+        }
+
+        public string genLon()
+        {
+            string lon = "";
+            Random random = new Random();
+            int first = -180;
+            int last = 180;
+            lon = Convert.ToString(random.Next(first, last));
+            return lon;
+        }
+
+        public async Task genResponse()
+        {
+            HttpClient httpClient = new HttpClient();
+            string response_data = $"https://api.openweathermap.org/data/2.5/weather?lat={lat_}&lon={lon_}&appid={API_key}";
+            using HttpResponseMessage response = await httpClient.GetAsync(response_data);
+            string content = await response.Content.ReadAsStringAsync(); // получение ответа
+            var deserializeObject = JsonConvert.DeserializeObject<dynamic>(content)!;
+            Country = deserializeObject.sys.country;
+            Name = deserializeObject.name;
+            Temp = deserializeObject.main.temp;
+            Description = deserializeObject.weather[0].description;
+        }
+
+        public void printInfo()
+        {
+            Console.WriteLine("Start printing a content: ");
+            Console.WriteLine();
+            Console.WriteLine($"Country: {Country}");
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Temp: {Temp}");
+            Console.WriteLine($"Description: {Description}");
+            Console.WriteLine();
+            Console.WriteLine("End printing a content: ");
+            Console.WriteLine("###############################################");
+        }
+        public int CompareTo(Weather other)
+        {
+            if (Temp.CompareTo(other.Temp) != 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+    }
+}
+
